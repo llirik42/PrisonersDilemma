ec3606432c74a521d5e30a2cbf5d11a227372ec2
llirik42 <kondrenkokp@yandex.ru>
2022-10-18 00:46:09 +0700

    Attempt of creating smart factory)

--- src/strategies_factory.cpp
+++ src/strategies_factory.cpp
@@ -1,22 +1,45 @@
 #include "strategies_factory.h"
 
+
+
+
+
 template<typename Type>
 std::unique_ptr<Strategy> create(){
     return std::make_unique<Type>();
 }
 
 StrategiesFactory::StrategiesFactory(){
-    _info.push_back(StrategyInfo({"Constant", "Description 1"}));
-    _info.push_back(StrategyInfo({"Random", "Description 2"}));
-    _info.push_back(StrategyInfo({"Periodic", "Description 3"}));
+    _info.push_back(StrategyInfo({
+        StrategyDescription({"Constant", "Description 1"}),
+        create<ConstantStrategy>
+    }));
+
+    _info.push_back(StrategyInfo({
+        StrategyDescription({"Random", "Description 2"}),
+        create<RandomStrategy>
+    }));
+
+    _info.push_back(StrategyInfo({
+        StrategyDescription({"Periodic", "Description 3"}),
+        create<PeriodicStrategy>
+                                 }));
+
+    for (const auto& i : _info){
+        _description.push_back(i.description);
+    }
 }
 
-
-
-
-
-
 std::unique_ptr<Strategy> StrategiesFactory::create_strategy(const std::string& title){
+    for (const auto& i : _info){
+        if (i.description.title == title){
+            return i.create();
+        }
+    }
+
+    return nullptr;
+
+    /*
     if (title == "Constant"){
         return std::make_unique<ConstantStrategy>();
     }
@@ -29,9 +52,9 @@ std::unique_ptr<Strategy> StrategiesFactory::create_strategy(const std::string& 
         return std::make_unique<PeriodicStrategy>();
     }
 
-    return nullptr;
+    return nullptr;*/
 }
 
-const std::vector<StrategyInfo>& StrategiesFactory::get_info_about_strategies(){
-    return _info;
+const std::vector<StrategyDescription>& StrategiesFactory::get_strategies_description(){
+    return _description;
 }
--- src/strategies_factory.h
+++ src/strategies_factory.h
@@ -5,24 +5,24 @@
 #include <memory>
 #include "strategy.h"
 
-class StrategiesFactory;
-
-class StrategyInfo{
-    friend StrategiesFactory;
-private:
-    std::unique_ptr<Strategy> (*create)();
-public:
+struct StrategyDescription{
     std::string title;
     std::string description;
 };
 
+struct StrategyInfo{
+    StrategyDescription description;
+    std::unique_ptr<Strategy> (*create)(){};
+};
+
 class StrategiesFactory{
 private:
+    std::vector<StrategyDescription> _description;
     std::vector<StrategyInfo> _info;
 public:
     StrategiesFactory();
 
-    const std::vector<StrategyInfo>& get_info_about_strategies();
+    const std::vector<StrategyDescription>& get_strategies_description();
 
     std::unique_ptr<Strategy> create_strategy(const std::string& title);
 };
