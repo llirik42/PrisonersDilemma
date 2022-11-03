ca96c60f193025ce2a0ff8bc404e4883eb77aaf1
llirik42 <kondrenkokp@yandex.ru>
2022-11-03 23:48:42 +0700

    Minor changes in matrices

--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -11,7 +11,6 @@ TEST(MatrixTest, test0){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// example matrix
 TEST(MatrixTest, test1){
     const char* path = "../matrices/matrix_1";
     const Matrix matrix = Matrix(path);
@@ -19,7 +18,6 @@ TEST(MatrixTest, test1){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// example matrix
 TEST(MatrixTest, test2){
     const char* path = "../matrices/matrix_2";
     const Matrix matrix = Matrix(path);
@@ -37,7 +35,6 @@ TEST(MatrixTest, test2){
     EXPECT_EQ(matrix[DDD], Row({1, 1, 1}));
 }
 
-// correct matrix with only positive numbers
 TEST(MatrixTest, test3){
     const char* path = "../matrices/matrix_3";
     const Matrix matrix = Matrix(path);
@@ -55,7 +52,6 @@ TEST(MatrixTest, test3){
     EXPECT_EQ(matrix[DDD], Row({5, 5, 5}));
 }
 
-// correct matrix with only negative numbers
 TEST(MatrixTest, test4){
     const char* path = "../matrices/matrix_4";
     const Matrix matrix = Matrix(path);
@@ -73,7 +69,6 @@ TEST(MatrixTest, test4){
     EXPECT_EQ(matrix[DDD], Row({-5, -5, -5}));
 }
 
-// correct matrix with positive and negative numbers
 TEST(MatrixTest, test5){
     const char* path = "../matrices/matrix_5";
     const Matrix matrix = Matrix(path);
@@ -91,7 +86,6 @@ TEST(MatrixTest, test5){
     EXPECT_EQ(matrix[DDD], Row({-5, -5, -5}));
 }
 
-// first inequality is wrong (3 * c0 > 2 * c1 + d0)
 TEST(MatrixTest, test6){
     const char* path = "../matrices/matrix_6";
     const Matrix matrix = Matrix(path);
@@ -100,7 +94,6 @@ TEST(MatrixTest, test6){
     EXPECT_FALSE(matrix.is_consistent());
 }
 
-// third inequality is wrong (d0 > c0 > d1 > c1 > d2 > c2)
 TEST(MatrixTest, test7){
     const char* path = "../matrices/matrix_7";
     const Matrix matrix = Matrix(path);
@@ -109,7 +102,6 @@ TEST(MatrixTest, test7){
     EXPECT_FALSE(matrix.is_consistent());
 }
 
-// matrix is not symmetric (in some especial way)
 TEST(MatrixTest, test8){
     const char* path = "../matrices/matrix_8";
     const Matrix matrix = Matrix(path);
@@ -118,7 +110,6 @@ TEST(MatrixTest, test8){
     EXPECT_FALSE(matrix.is_consistent());
 }
 
-// correct matrix with only positive numbers and unusual order of rows
 TEST(MatrixTest, test9){
     const char* path = "../matrices/matrix_9";
     const Matrix matrix = Matrix(path);
@@ -136,7 +127,6 @@ TEST(MatrixTest, test9){
     EXPECT_EQ(matrix[DDD], Row({5, 5, 5}));
 }
 
-// correct matrix with spaces, empty lines, negative numbers and comments
 TEST(MatrixTest, test10){
     const char* path = "../matrices/matrix_10";
     const Matrix matrix = Matrix(path);
@@ -154,7 +144,6 @@ TEST(MatrixTest, test10){
     EXPECT_EQ(matrix[DDD], Row({-5, -5, -5}));
 }
 
-// unsupported comment section (/* */) is considered as error
 TEST(MatrixTest, test11){
     const char* path = "../matrices/matrix_11";
     const Matrix matrix = Matrix(path);
@@ -162,7 +151,6 @@ TEST(MatrixTest, test11){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// unsupported comment (/) is considered as error
 TEST(MatrixTest, test12){
     const char* path = "../matrices/matrix_12";
     const Matrix matrix = Matrix(path);
@@ -170,7 +158,6 @@ TEST(MatrixTest, test12){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// incorrect number of rows (9 instead of 8) though row was duplicated
 TEST(MatrixTest, test13){
     const char* path = "../matrices/matrix_13";
     const Matrix matrix = Matrix(path);
@@ -178,7 +165,6 @@ TEST(MatrixTest, test13){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// incorrect number of rows (7 instead of 8)
 TEST(MatrixTest, test14){
     const char* path = "../matrices/matrix_14";
     const Matrix matrix = Matrix(path);
@@ -186,7 +172,6 @@ TEST(MatrixTest, test14){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// no space between letters in row
 TEST(MatrixTest, test15){
     const char* path = "../matrices/matrix_15";
     const Matrix matrix = Matrix(path);
@@ -194,7 +179,6 @@ TEST(MatrixTest, test15){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// incorrect number of letters in a row (2 instead of 3)
 TEST(MatrixTest, test16){
     const char* path = "../matrices/matrix_16";
     const Matrix matrix = Matrix(path);
@@ -202,7 +186,6 @@ TEST(MatrixTest, test16){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// incorrect number of letters in a row (4 instead of 3)
 TEST(MatrixTest, test17){
     const char* path = "../matrices/matrix_17";
     const Matrix matrix = Matrix(path);
@@ -210,7 +193,6 @@ TEST(MatrixTest, test17){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// incorrect number of numbers in a row (2 instead of 3)
 TEST(MatrixTest, test18){
     const char* path = "../matrices/matrix_18";
     const Matrix matrix = Matrix(path);
@@ -218,7 +200,6 @@ TEST(MatrixTest, test18){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// incorrect number of numbers in a row (4 instead of 3)
 TEST(MatrixTest, test19){
     const char* path = "../matrices/matrix_19";
     const Matrix matrix = Matrix(path);
@@ -226,7 +207,6 @@ TEST(MatrixTest, test19){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// random line (not a comment)
 TEST(MatrixTest, test20){
     const char* path = "../matrices/matrix_20";
     const Matrix matrix = Matrix(path);
@@ -234,7 +214,6 @@ TEST(MatrixTest, test20){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// (--a) is not supported in a table
 TEST(MatrixTest, test21){
     const char* path = "../matrices/matrix_21";
     const Matrix matrix = Matrix(path);
@@ -242,7 +221,6 @@ TEST(MatrixTest, test21){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// random symbols in the table
 TEST(MatrixTest, test22){
     const char* path = "../matrices/matrix_22";
     const Matrix matrix = Matrix(path);
@@ -250,7 +228,6 @@ TEST(MatrixTest, test22){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// 2 rows in one line
 TEST(MatrixTest, test23){
     const char* path = "../matrices/matrix_23";
     const Matrix matrix = Matrix(path);
@@ -258,14 +235,12 @@ TEST(MatrixTest, test23){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// row that was split in 2 lines
 TEST(MatrixTest, test24){
     const char* path = "../matrices/matrix_24";
     const Matrix matrix = Matrix(path);
 
     EXPECT_TRUE(matrix.has_error());
 }
-// number "00" in rows
 TEST(MatrixTest, test25){
     const char* path = "../matrices/matrix_25";
     const Matrix matrix = Matrix(path);
@@ -273,7 +248,6 @@ TEST(MatrixTest, test25){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// number "-0" in rows
 TEST(MatrixTest, test26){
     const char* path = "../matrices/matrix_26";
     const Matrix matrix = Matrix(path);
@@ -281,7 +255,6 @@ TEST(MatrixTest, test26){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// non-integer numbers
 TEST(MatrixTest, test27){
     const char* path = "../matrices/matrix_27";
     const Matrix matrix = Matrix(path);
@@ -289,7 +262,6 @@ TEST(MatrixTest, test27){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// incorrect number (07) in first row (it's supposed that numbers must be "0" or non-zero)
 TEST(MatrixTest, test28){
     const char* path = "../matrices/matrix_28";
     const Matrix matrix = Matrix(path);
@@ -297,7 +269,6 @@ TEST(MatrixTest, test28){
     EXPECT_TRUE(matrix.has_error());
 }
 
-// 8 rows, but first row is doubled so one row is missing
 TEST(MatrixTest, test29){
     const char* path = "../matrices/matrix_29";
     const Matrix matrix = Matrix(path);
